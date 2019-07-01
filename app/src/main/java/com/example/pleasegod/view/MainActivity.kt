86d3dd883980bce192fe3e1e06e5874a6320ab65
hclee <dlgockd45@gmail.com>
2019-07-01 17:23:35 +0900

    Request permission ACCESS_FINE_LOCATION to user

--- app/src/main/java/com/example/pleasegod/view/MainActivity.kt
+++ app/src/main/java/com/example/pleasegod/view/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.example.pleasegod.view
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pleasegod.R
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val REQUEST_CODE: Int = 111
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, RestroomListActivity::class.java))
-        finish()
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        for(result in grantResults) {
+            if(result == PackageManager.PERMISSION_GRANTED) {
+                startActivity(Intent(this, RestroomListActivity::class.java))
+                finish()
+            }
+        }
     }
 }
