7578c1d5b36c9111822b9037f77bef6c416487d0
hclee <dlgockd45@gmail.com>
2019-07-02 00:46:13 +0900

    Show current location

--- app/src/main/java/com/example/pleasegod/view/RestroomMapActivity.kt
+++ app/src/main/java/com/example/pleasegod/view/RestroomMapActivity.kt
@@ -2,23 +2,40 @@ package com.example.pleasegod.view
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.pleasegod.R
+import com.example.pleasegod.model.entity.Restroom
+import com.example.pleasegod.viewmodel.RestroomViewModel
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 
-class RestroomMapActivity : AppCompatActivity(), OnMapReadyCallback {
+class RestroomMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener {
     companion object {
         val TAG: String = RestroomMapActivity::class.java.simpleName
+        val DEFAULT_ZOOM: Float = 15f
     }
 
+    private lateinit var mGoogleApiClient: GoogleApiClient
     private lateinit var mMap: GoogleMap
     private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var mCurrentLatLng: LatLng
+    private lateinit var mRestroomViewModel: RestroomViewModel
+    private val mRestroomList: MutableList<Restroom> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +45,75 @@ class RestroomMapActivity : AppCompatActivity(), OnMapReadyCallback {
             getMapAsync(this@RestroomMapActivity)
         }
 
+        init()
+        getRestroomList()
+    }
+
+    private fun init() {
+        mRestroomViewModel = ViewModelProviders.of(this).get(RestroomViewModel::class.java).apply {
+            mRestroomLiveData.observe(this@RestroomMapActivity, Observer {
+                mRestroomList.clear()
+                mRestroomList.addAll(it)
+            })
+        }
+        mGoogleApiClient = GoogleApiClient.Builder(this@RestroomMapActivity)
+            .addConnectionCallbacks(this@RestroomMapActivity)
+            .addOnConnectionFailedListener(this@RestroomMapActivity)
+            .addApi(LocationServices.API)
+            .build()
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun showCurrentLocation() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val locationResult: Task<Location> = (mFusedLocationProviderClient.lastLocation).apply {
+                addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        task.result?.let {
+                            mCurrentLatLng = LatLng(it.latitude, it.longitude)
+
+                            mMap.addMarker(
+                                MarkerOptions()
+                                    .title(getString(R.string.current_location))
+                                    .position(mCurrentLatLng)
+                            )
+                            mMap.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    mCurrentLatLng,
+                                    DEFAULT_ZOOM
+                                )
+                            )
+                        }
+                    } else {
+                        Log.d(TAG, "can't retrieve current location!")
+                        Log.d(TAG, task.exception?.message)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getRestroomList(pageIndex: Int = 1, pageSize: Int = 1000, sigunName: String = "고양시") {
+        mRestroomViewModel.getRestroomList(pageIndex, pageSize, sigunName)
+    }
+
+    override fun onConnected(connectionHint: Bundle?) {
+        Log.d(TAG, "onConnected()")
+    }
+
+    override fun onConnectionSuspended(cause: Int) {
+        Log.d(TAG, "onConnectionSuspended()")
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.d(TAG, "onConnectionFailed()")
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         Log.d(TAG, "onMapReady()")
 
         mMap = googleMap.apply {
-            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 isMyLocationEnabled = true
 //                mFusedLocationProviderClient.requestLocationUpdates(mLoca)
             }
@@ -45,6 +123,8 @@ class RestroomMapActivity : AppCompatActivity(), OnMapReadyCallback {
                 it.isZoomControlsEnabled = true
             }
         }
+
+        showCurrentLocation()
     }
 
     override fun onBackPressed() {
@@ -52,5 +132,11 @@ class RestroomMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         finish()
         overridePendingTransition(R.anim.animation_slide_from_left, R.anim.animation_slide_to_right)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mRestroomViewModel.clearDisposable()
     }
 }
