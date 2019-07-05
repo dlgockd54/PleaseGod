6cba542525d77cc00befc741f245b6fcfdde4e09
hclee <dlgockd45@gmail.com>
2019-07-05 16:56:11 +0900

    Replace DialogPlus with BottomSheetDialog

--- app/src/main/java/com/example/pleasegod/view/RestroomMapActivity.kt
+++ app/src/main/java/com/example/pleasegod/view/RestroomMapActivity.kt
@@ -34,9 +34,9 @@ import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.miguelcatalan.materialsearchview.MaterialSearchView
-import com.orhanobut.dialogplus.DialogPlus
 import kotlinx.android.synthetic.main.activity_restroom_map.*
 import kotlinx.android.synthetic.main.bottom_sheet_searched_restroom.view.rv_searched_restroom_list
+import kotlinx.android.synthetic.main.item_restroom_information.view.*
 
 class RestroomMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
     GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnMarkerClickListener {
@@ -393,21 +393,71 @@ class RestroomMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiCl
     }
 
     private fun showRestroomInformationDialog() {
-        if (mRestroomInformationAdapter == null) {
-            mRestroomInformationAdapter = RestroomInformationAdapter(this@RestroomMapActivity, mCurrentLatLng, mClickedRestroom)
-        } else {
-            mRestroomInformationAdapter?.mRestroom = mClickedRestroom
+        val restroomInformationView: View =
+            LayoutInflater.from(this@RestroomMapActivity).inflate(R.layout.item_restroom_information, null).apply {
+                tv_restroom_info_name.text = mClickedRestroom.pbctlt_plc_nm
+                tv_restroom_info_open_time.text = "개방 시간: ${mClickedRestroom.open_tm_info}"
+                tv_restroom_info_road_name_address.text = mClickedRestroom.refine_roadnm_addr
+
+                if (mClickedRestroom.refine_wgs84_lat != null && mClickedRestroom.refine_wgs84_logt != null) {
+                    val distanceArr: FloatArray = FloatArray(1)
+
+                    Location.distanceBetween(
+                        mCurrentLatLng.latitude,
+                        mCurrentLatLng.longitude,
+                        mClickedRestroom.refine_wgs84_lat!!.toDouble(),
+                        mClickedRestroom.refine_wgs84_logt!!.toDouble(),
+                        distanceArr
+                    )
+
+                    tv_restroom_info_distance.text = "현재 위치로부터의 직선거리: ${distanceArr[0]}m"
+                } else {
+                    tv_restroom_info_distance.visibility = View.GONE
+                }
+
+                if (mClickedRestroom.male_female_toilet_yn == null) {
+                    tv_restroom_info_male_female_toilet.visibility = View.GONE
+                } else {
+                    tv_restroom_info_male_female_toilet.text = "남녀 공용화장실 여부: ${mClickedRestroom.male_female_toilet_yn}"
+                }
+
+                if (mClickedRestroom.manage_inst_nm == null) {
+                    tv_restroom_info_manage_inst_name.visibility = View.GONE
+                } else {
+                    tv_restroom_info_manage_inst_name.text = "관리기관: ${mClickedRestroom.manage_inst_nm}"
+                }
+
+                if (mClickedRestroom.manage_inst_telno == null) {
+                    tv_restroom_info_manage_inst_tel_number.visibility = View.GONE
+                } else {
+                    tv_restroom_info_manage_inst_tel_number.text = "전화번호: ${mClickedRestroom.manage_inst_telno}"
+                }
+
+                if (mClickedRestroom.male_dspsn_wtrcls_cnt == null) {
+                    tv_restroom_info_male_dspsn_wtrcls_cnt.visibility = View.GONE
+                } else {
+                    tv_restroom_info_male_dspsn_wtrcls_cnt.text =
+                        "남성용-장애인용 대변기 수: ${mClickedRestroom.male_dspsn_wtrcls_cnt}"
+                }
+
+                if (mClickedRestroom.male_dspsn_uil_cnt == null) {
+                    tv_restroom_info_male_dspsn_uil_cnt.visibility = View.GONE
+                } else {
+                    tv_restroom_info_male_dspsn_uil_cnt.text = "남성용-장애인용 소변기 수: ${mClickedRestroom.male_dspsn_uil_cnt}"
+                }
+
+                if (mClickedRestroom.female_dspsn_wtrcls_cnt == null) {
+                    tv_restroom_info_female_dspsn_wtrcls_cnt.visibility = View.GONE
+                } else {
+                    tv_restroom_info_female_dspsn_wtrcls_cnt.text =
+                        "여성용-장애인용 대변기 수: ${mClickedRestroom.female_dspsn_wtrcls_cnt}"
+                }
+            }
+        val restroomInformationDialog: BottomSheetDialog = BottomSheetDialog(this@RestroomMapActivity).apply {
+            setContentView(restroomInformationView)
         }
 
-        mRestroomInformationAdapter?.notifyDataSetChanged()
-
-        val dialog: DialogPlus = DialogPlus.newDialog(this@RestroomMapActivity)
-                .setOnItemClickListener { dialog, item, view, position -> }
-                .setOnDismissListener { }
-                .setAdapter(mRestroomInformationAdapter)
-                .create()
-
-        dialog.show()
+        restroomInformationDialog.show()
     }
 
     private fun getRestroomList(pageIndex: Int = 1, pageSize: Int = 1000, sigunName: String = "고양시") {
